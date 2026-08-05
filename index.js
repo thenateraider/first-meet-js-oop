6aa8217a6ec469976f8891c8801c2e59f6e7fa00
Thenateraider <Ekaluck.musik@gmail.com>
2026-08-05 14:20:04 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,92 @@
+class Animal {
+    constructor(name, species) {
+        this.name = name;
+        this.species = species;
+        this.hunger = 50;
+    }
+    makeSound() {
+         console.log(`${this.name} Make a sound`)
+    }
+    eat() {
+        this.hunger = this.hunger - 10;
+        console.log(
+            `${this.name} the ${this.species} ate
+         Hunger level is now ${this.hunger}.`
+        );
+    }
+
+}
+
+const leo = new Animal("Leo", "Lion");
+
+console.log(leo);
+
+console.log(leo.hunger);
+
+leo.eat();
+
+console.log(leo.hunger);
+
+class Mammal extends Animal {
+    constructor(name, species, furColor) {
+        super(name,species);
+        this.furColor = furColor;
+    }
+
+    groom() {
+        console.log(`${this.name} is brushing thier ${this.furColor} fur.`);
+    }
+}
+
+class Birds extends Animal {
+    constructor(name, species, wingspan) {
+        super(name,species);
+        this.wingspan = wingspan;
+    }
+
+    makeSound() {
+        console.log(`${this.name} chirps: Tweet! Tweet!`)
+    }
+
+}
+
+const zazu = new Birds("Zazu","Hornbill", "2 feet");
+const baloo = new Mammal("Baloo","Bear","brown");
+
+zazu.makeSound();
+baloo.groom();
+
+class Reptile extends Animal {
+    constructor(name, species, isVenomous) {
+        super(name, species);
+        this.isVenomous = isVenomous; 
+    }
+
+    baskInSun() {
+        console.log(`${this.name} the ${this.species} is warm-bloodedly soaking in the sun.`);
+    }
+}
+
+
+class Mutant extends Animal {
+    constructor(name, species, mutantPower) {
+        super(name, species);
+        this.mutantPower = mutantPower; 
+    }
+    usePower() {
+        console.log(`${this.name} is a ${this.species} has power: ${this.mutantPower}!`);
+    }
+
+    makeSound() {
+        console.log(`${this.name} shouts: I'm back baby!`);
+    }
+}
+
+const pascal = new Reptile("Pascal", "Chameleon", false);
+const wolverine = new Mutant("Logan", "Human/Mutant - X-MEN ", "Adamantium Claws & Healing Factor");
+
+
+pascal.baskInSun();
+wolverine.makeSound();
+wolverine.usePower();
+
